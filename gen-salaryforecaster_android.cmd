8c44ea617b9461b87660505ef625b2c9c970b720
alexy <alexy@local.yurion.ru>
2020-01-07 00:05:41 +0300

    Изменены пути для сгенерированных проектов под Android

--- gen-salaryforecaster_android.cmd
+++ gen-salaryforecaster_android.cmd
@@ -1,3 +1,3 @@
 @ECHO OFF
 call YBuild\vs2017var.cmd
-YBuild\libgen.py SALARYFORECASTER_ANDROID --solution-name "SalaryForecaster.Android" --custom-build-dir-suffix "_ANDROID" -G "Visual Studio 15"
+YBuild\libgen.py SALARYFORECASTER_ANDROID --solution-name "SalaryForecaster.Android" -G "Visual Studio 15"
